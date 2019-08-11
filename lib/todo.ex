2a66db48dc0311e0189e76b11e88ddf4c72a7da4
ain2108 <ain2108@columbia.edu>
2019-08-11 00:54:12 -0400

    added get_entries funciton

--- lib/todo.ex
+++ lib/todo.ex
@@ -12,14 +12,23 @@ defmodule TodoList do
       %{}
       iex> TodoList.new() |> TodoList.add_entry(~D(2019-07-05), "Get married")
       %{~D(2019-07-05) => ["Get married"]}
+      iex> TodoList.new() |> TodoList.add_entry(~D(2019-07-05), "Get married") |> TodoList.entries(~D(2019-07-05))
+      ["Get married"]
 
   """
 
+  @spec new() :: map
   def new do
     %{}
   end
-  
+
+  @spec add_entry(map, any, any) :: map
   def add_entry(todo_list, date, title) do
     Map.update(todo_list, date, [title], fn titles -> [title | titles] end)
+  end
+
+  @spec entries(map, any) :: any
+  def entries(todo_list, date) do
+    Map.get(todo_list, date, [])
   end
 end
--- test/todo_test.exs
+++ test/todo_test.exs
@@ -5,4 +5,10 @@ defmodule TodoListTest do
   test "creates new todo list" do
     assert TodoList.new() == %{}
   end
+
+  test "adds and get a entry to list" do
+
+    assert (TodoList.new() |> TodoList.add_entry(~D(2019-07-05), "Get married") |> TodoList.entries(~D(2019-07-05))) == ["Get married"]
+
+  end
 end
